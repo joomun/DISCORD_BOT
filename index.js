d235cc8b75225599cc5dc11bcd81d6c69acdfa22
Joomun <Joomun@Desktop.com>
2025-10-26 02:00:57 +0400

    Initial commit without token

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+require('dotenv').config();
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong!');
+  }
+});
+
+client.login(TOKEN);
